dc8612559fcaa17f44933d7d17c6744ee6508b7c
Alireza Heydari <alireza97hi@gmail.com>
2019-01-18 12:30:17 +0330

    keywords added to lex

--- main.py
+++ main.py
@@ -4,12 +4,57 @@ import ply.lex as lex
 tokens = (
 'NUMBER',
 'LETTER',
+'OPENING_BRACKET',
+'CLOSING_BRACKET',
+'OPENING_PARENTHESES',
+'CLOSING_PARENTHESES',
+'OPENING_BRACE',
+'CLOSING_BRACE',
+'PLUSPLUS',
+'MINUSMINUS',
+'EQUAL',
+'PLUSEQUAL',
+'MINUSEQUAL',
+'TIMESEQUAL',
+'DIVIDEEQUAL',
+'LESSEQUAL',
+'GREATEREQUAL',
+'EQUALEQUAL',
+'LESS_THAN',
+'GREATER_THAN',
+'NOTEQUAL',
+'SEMICOLON',
+'DOUBLE_DOT',
 'PLUS',
 'MINUS',
 'TIMES',
 'DIVIDE',
-'LPAREN',
-'RPAREN',
+'PERCENTAGE',
+'QUESTION_MARK',
+'LOGINCAL_AND',
+'LOGINCAL_OR',
+'TILDA',
+'AND',
+'OR',
+'STATIC_KW',
+'BOOLEAN_KW',
+'CHARACTER_KW',
+'INTEGER_KW',
+'CHAR_KW',
+'BOOL_KW',
+'INT_KW',
+'VOID_KW',
+'IF_KW',
+'OTHER_KW',
+'TILL_KW',
+'COMEBACK_KW',
+'GIVEBACK_KW',
+'CONTINUE_KW',
+'THEN_KW',
+'ELSE_KW',
+'CONST_KW',
+'TRUE_KW',
+'FALSE_KW',
 )
 
 # Regular expression rules for simple tokens
@@ -17,8 +62,56 @@ t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_PLUSPLUS  = r'\+\+'
+t_MINUSMINUS  = r'\-\-'
+t_EQUAL  = r'='
+t_PLUSEQUAL  = r'\+='
+t_MINUSEQUAL  = r'-='
+t_TIMESEQUAL  = r'\*='
+t_DIVIDEEQUAL  = r'/='
+t_LESSEQUAL  = r'<='
+t_GREATEREQUAL  = r'>='
+t_EQUALEQUAL  = r'=='
+t_LESS_THAN  = r'<'
+t_GREATER_THAN  = r'>'
+t_NOTEQUAL  = r'\!='
+t_SEMICOLON  = r';'
+t_PERCENTAGE  = r'%'
+t_QUESTION_MARK  = r'\?'
+t_LOGINCAL_AND  = r'&&'
+t_LOGINCAL_OR  = r'\|\|'
+t_TILDA  = r'~'
+t_AND  = r'and'
+t_OR  = r'or'
+t_DOUBLE_DOT  = r':'
+t_OPENING_BRACKET = r'\['
+t_CLOSING_BRACKET = r'\]'
+t_OPENING_PARENTHESES = r'\('
+t_CLOSING_PARENTHESES = r'\)'
+t_OPENING_BRACE = r'\{'
+t_CLOSING_BRACE = r'\}'
+
+reserved = {
+    'static': 'STATIC_KW',
+    'boolean': 'BOOLEAN_KW',
+    'character': 'CHARACTER_KW',
+    'integer': 'INTEGER_KW',
+    'char': 'CHAR_KW',
+    'bool': 'BOOL_KW',
+    'int': 'INT_KW',
+    'void': 'VOID_KW',
+    'if': 'IF_KW',
+    'other': 'OTHER_KW',
+    'till': 'TILL_KW',
+    'comeback': 'COMEBACK_KW',
+    'giveback': 'GIVEBACK_KW',
+    'continue': 'CONTINUE_KW',
+    'then': 'THEN_KW',
+    'else': 'ELSE_KW',
+    'const': 'CONST_KW',
+    'true': 'TRUE_KW',
+    'false': 'FALSE_KW',
+}
 
 # A regular expression rule with some action code
 def t_NUMBER(t):
@@ -27,6 +120,7 @@ def t_NUMBER(t):
 
 def t_LETTER(t):
     r'[a-zA-Z]+'
+    t.type = reserved.get(t.value, 'LETTER')
     return t
 
 # Define a rule so we can track line numbers
@@ -51,8 +145,8 @@ lexer = lex.lex()
 
 # Test it out
 data = '''
-LER + 4 * 10
-+ -20 *2
+LER + [4] * 10 static && wow : / */ -= += 
++ -20 *2 comeback *= awer giveback
 '''
 
 # Give the lexer some input
