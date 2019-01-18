e78280a8bfa58e92e224b29b6dd21a34fa8e89a6
Alireza Heydari <alireza97hi@gmail.com>
2019-01-18 11:34:29 +0330

    initla commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import ply.lex as lex
+ 
+ # List of token names.   This is always required
+tokens = (
+'NUMBER',
+'LETTER',
+'PLUS',
+'MINUS',
+'TIMES',
+'DIVIDE',
+'LPAREN',
+'RPAREN',
+)
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'[1-9]+[0-9]*'
+    return t
+
+def t_LETTER(t):
+    r'[a-zA-Z]+'
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+
+
+
+
+# Test it out
+data = '''
+LER + 4 * 10
++ -20 *2
+'''
+
+# Give the lexer some input
+lexer.input(data)
+ 
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
